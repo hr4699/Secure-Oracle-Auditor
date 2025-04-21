c278045d16c3c9ae7dc81f9e6516a16665a6e6dd
hr4699 <k214699@nu.edu.pk>
2025-04-21 12:27:41 +0500

    Resolved merge conflicts

--- main.py
+++
@@ -1,8 +0,0 @@
-import cx_Oracle
-
-dsn_tns = cx_Oracle.makedsn("localhost", 1521, service_name="XE")  # Change "XE" if needed
-connection = cx_Oracle.connect(user="k214699", password="123", dsn=dsn_tns)
-
-print("Connected to Oracle Database Successfully!")
-
-connection.close()
